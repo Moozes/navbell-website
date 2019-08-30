bf3dfc624c03037dfd14cf1cc1b7339b0a8cc060
Moozes <48410547+Moozes@users.noreply.github.com>
2019-08-30 22:01:43 +0100

    prevent the refresh of stratQuiz anty cheat

--- startQuiz.php
+++ startQuiz.php
@@ -1,10 +1,9 @@
 <?php
-  // session_start();
-  // $_SESSION ['refreshCount']++;
-  // if($_SESSION['refreshCount'] != 1) {
-  //   header('location: main.php');
-  // }
   session_start();
+  $_SESSION ['refreshCount']++;
+  if($_SESSION['refreshCount'] != 1) {
+    header('location: main.php');
+  }
   include('./functions/functions.php');
   include('pages/main/get_profile_info.php');
   include('pages/startQuiz/makeQuestionsTable.php');
